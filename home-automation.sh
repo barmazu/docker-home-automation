5d6b03ab97d8cd3e0ac8c738e19556796c7465e9
barmazu <barmazu@gmail.com>
2021-03-14 17:17:21 +0100

    Make semi-upgrades possible

--- home-automation.sh
+++ home-automation.sh
@@ -10,16 +10,16 @@
 #
 if [[ "${UID}" -ne 0 ]]
 then
-    >&2 echo "This script must be run as root, re-exec with sudo..."
+    f__echo_err "This script must be run as root, re-exec with sudo..."
     exec sudo "${0}" "${@}"
     exit
 fi
 
 if [[ ! -x "$(command -v docker 2>/dev/null)" ]]
 then
-    >&2 echo "'docker' binary not found or not in the PATH"
-    >&2 echo "Make sure docker-ce engine is installed and working"
-    >&2 echo "See: https://docs.docker.com/install"
+    f__echo_err "'docker' binary not found or not in the PATH"
+    f__echo_err "Make sure docker-ce engine is installed and working"
+    f__echo_err "See: https://docs.docker.com/install"
     exit ${FAILURE}
 fi
 
@@ -27,80 +27,120 @@ if [[ -x "$(command -v docker-compose 2>/dev/null)" ]]
 then
     DOCKER_COMPOSE_BIN=$(command -v docker-compose)
 else
-    >&2 echo "'docker-compose' binary not found or not in the PATH"
-    >&2 echo "See: https://github.com/docker/compose/releases"
+    f__echo_err "'docker-compose' binary not found or not in the PATH"
+    f__echo_err "See: https://github.com/docker/compose/releases"
     exit ${FAILURE}
 fi
 
 #
 # Is compose file present?
 #
-if [[ ! -e ${DOCKER_COMPOSE_FILE} ]]
+if [[ ! -e ${DOCKER_COMPOSE_FILE} ]] || [[ ${1} == "setup" ]]
 then
-    echo "File ${DOCKER_COMPOSE_FILE} does not exist"
-    echo "Ignoring given parameters, going into initial setup mode..."
+    f__echo "File ${DOCKER_COMPOSE_FILE} does not exist or setup explicitly requested"
+    f__echo "Going into initial setup mode..."
+    shift 1
     INITIAL_SETUP=${TRUE}
 fi
 
 if [[ ${INITIAL_SETUP} -eq ${TRUE} ]]
 then
-
+    
+    #
+    # Create files with some restrictions
+    #
+    umask 026
+    
     #
     # Setup directory structure
     #
     [[ ! -e  ${PROJECT_PATH} ]] && mkdir -p ${PROJECT_PATH}
-    for DIR in ${HOME_ASSISTANT_DIR} ${MQTT_DIR} ${ZIGBEE2MQTT_DIR}
+    
+    #
+    # Copy initial and template files to target project path
+    #
+    for DIR in ${HOME_ASSISTANT_DIR} ${ZIGBEE2MQTT_DIR} ${MQTT_DIR}
     do
-        if [[ ! -e "${DIR}" ]]
+        # No need to copy zigbee2mqtt if we are not using it
+        if [[ "${DIR}"  =~ zigbee2mqtt ]] && [[ -z ${ZB_DEVICE_PATH} ]]
         then
-            (
-              umask 022
-              cp -r ${SRC_CONFIG_DIR}/"$(basename ${DIR})" ${PROJECT_PATH}
-              if [[ $? -eq ${SUCCESS} ]]
-              then
-                  echo "Directory ${DIR} has been setup successfully."
-              else
-                  >&2 echo "Error: Unable to set up directory: ${DIR}"
-                  exit ${FAILURE}
-              fi
-            )
+            f__echo_warn "WARNING: Skipping ${DIR} as ZB_DEVICE_PATH is empty"
+            continue
+        fi
+        cp -r ${SRC_CONFIG_DIR}/"$(basename ${DIR})" ${PROJECT_PATH}
+        if [[ $? -eq ${SUCCESS} ]]
+        then
+            f__echo_ok "Directory ${DIR} has been setup successfully."
+        else
+            f__echo_err "Error: Unable to set up directory: ${DIR}"
+            exit ${FAILURE}
         fi
     done
-
+    
+    #
+    # Setup initial files
+    #
+    INITIAL_FILES="$(find ${PROJECT_PATH} -type f -name "*-initial")"
+    for INIT_FILE in ${INITIAL_FILES}
+    do
+        FILE="${INIT_FILE%%-initial}"
+        if [[ ! -e "${FILE}" ]]
+        then
+            f__echo "Creating ${FILE} file using initial file..."
+            mv "${INIT_FILE}" "${FILE}"
+            if [[ $? -eq ${SUCCESS} ]]
+            then
+                f__echo_ok "File ${FILE} has been created successfully."
+            else
+                f__echo_err "Error: Unable to set up file: ${FILE}"
+                exit ${FAILURE}
+            fi
+        fi
+        rm -rf "${INIT_FILE}"
+    done
+    
     #
     # Update template YAML files with config values
     #
     TEMPLATE_FILES="$(find ${PROJECT_PATH} -type f -name "*-template")"
-    for FILE in ${TEMPLATE_FILES}
+    for TEMPL_FILE in ${TEMPLATE_FILES}
     do
-        echo "Updating ${FILE} file..."
-        envsubst <"${FILE}" >"${FILE%%-template}"
-        if [[ $? -eq ${SUCCESS} ]]
+        FILE="${TEMPL_FILE%%-template}"
+        if [[ ! -e "${FILE}" ]]
         then
-            echo "Template file ${FILE} has been updated successfully."
-        else
-            >&2 echo "Error: Unable to set up template faile: ${FILE}"
-            exit ${FAILURE}
+            f__echo "Creating ${FILE} file using template..."
+            envsubst <"${TEMPL_FILE}" >"${FILE}"
+            if [[ $? -eq ${SUCCESS} ]]
+            then
+                f__echo_ok "File ${FILE} has been created successfully."
+            else
+                f__echo_err "Error: Unable to set up file: ${FILE}"
+                exit ${FAILURE}
+            fi
         fi
-        rm -rf "${FILE}"
+        rm -rf "${TEMPL_FILE}"
     done
 
     #
-    # Setup MQTT password
+    # Hash MQTT password
     #
     if [[ ! -e ${MQTT_DIR}/data/passwd-template ]]
     then
-        echo "Setting up MQTT password..."
-        docker run --rm \
-            -v ${MQTT_DIR}/data/passwd:/mosquitto/data/passwd \
-            eclipse-mosquitto \
-                mosquitto_passwd -U /mosquitto/data/passwd >/dev/null 2>&1
-        if [[ $? -eq ${SUCCESS} ]]
+        # Make sure we are hashing plain text password
+        if grep -q "${MQTT_PASSWORD}" ${MQTT_DIR}/data/passwd
         then
-            echo "MQTT credentials have been setup successfully."
-        else
-            >&2 echo "Error: Unable to set up MQTT credentials."
-            exit ${FAILURE}
+            f__echo "Hashing MQTT plain text password..."
+            docker run --rm \
+                -v ${MQTT_DIR}/data/passwd:/mosquitto/data/passwd \
+                eclipse-mosquitto \
+                    mosquitto_passwd -U /mosquitto/data/passwd >/dev/null 2>&1
+            if [[ $? -eq ${SUCCESS} ]]
+            then
+                f__echo_ok "MQTT password has been hashed successfully."
+            else
+                f__echo_err "Error: Unable to set up MQTT credentials."
+                exit ${FAILURE}
+            fi
         fi
     fi
 
@@ -112,23 +152,23 @@ then
         envsubst <"${SRC_CONFIG_DIR}/${DOCKER_COMPOSE_FILE}-template" >"./${DOCKER_COMPOSE_FILE}"
         if [[ $? -eq ${SUCCESS} ]]
         then
-            echo "Project configuration file: ${DOCKER_COMPOSE_FILE} created."
+            f__echo_ok "Project configuration file: ${DOCKER_COMPOSE_FILE} created."
         else
-            >&2 echo "Error: Unable to create ${DOCKER_COMPOSE_FILE} file."
+            f__echo_err "Error: Unable to create ${DOCKER_COMPOSE_FILE} file."
             exit ${FAILURE}
         fi
     fi
     
     #
-    # Re-exec if parameters were givien 
+    # Re-exec if parameters were given 
     #
     if [[ $# -eq 1 ]] && [[ ! "$@" =~ "down" ]]
     then
-        echo "Restarting now with previously given parameters: $@"
+        f__echo "Restarting now with previously given parameters: $@"
         exec "$0" "$@"
         exit 
     else
-        echo "Now, start your Home Assistant with: ${0} up"
+        f__echo_ok "Now, start your Home Assistant with: ${0} up"
     fi
     
 else
@@ -144,14 +184,17 @@ else
             fi
             if [[ ${RC} -eq ${SUCCESS} ]]
             then
-                echo "Home Assistant ready at: http://$(hostname -I | cut -d' ' -f1):${HA_PORT}"
+                for HOST_IP in ${HOST_IP_LIST}
+                do
+                    f__echo_ok "Home Assistant ready at: http://${HOST_IP}:${HA_PORT}"
+                done
             else
-                >&2 echo "Error: Unable to create ${PROJECT_NAME} conatiners..."
+                f__echo_err "Error: Unable to create ${PROJECT_NAME} containers..."
             fi
             ;;
         down)
-            echo ""
-            echo "WARNING: You are about to EARSE docker containers, image and networks"
+            f__echo_warn "---"
+            f__echo_warn "WARNING: You are about to ERASE docker containers, image and networks"
             read -p "Are you sure? (y/n) " -r
             if [[ ${REPLY} =~ ^[Yy]$ ]]
             then
@@ -159,19 +202,19 @@ else
                 RC=$?
             fi
             unset REPLY
-            echo ""
-            echo "WARNING: You are about to EARSE persistent storage and configuration"
+            f__echo_warn "---"
+            f__echo_warn "WARNING: You are about to ERASE persistent storage and configuration"
             read -p "Are you sure? (y/n) " -r
             if [[ ${REPLY} =~ ^[Yy]$ ]]
             then
                 rm -rf ${PROJECT_PATH}
-                echo "Project storage ${PROJECT_PATH} removed."
+                f__echo "Project storage ${PROJECT_PATH} removed."
                 rm -rf ./${DOCKER_COMPOSE_FILE}
-                echo "Project configuration ${DOCKER_COMPOSE_FILE} removed"
+                f__echo "Project configuration ${DOCKER_COMPOSE_FILE} removed"
                 
             else
-                echo "Persistent storage ${PROJECT_PATH} path left unchanged."
-                echo "Project configuration ${DOCKER_COMPOSE_FILE} not removed."
+                f__echo "Persistent storage ${PROJECT_PATH} path left unchanged."
+                f__echo "Project configuration ${DOCKER_COMPOSE_FILE} not removed."
             fi
             ;;
         *)
